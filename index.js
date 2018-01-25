f45d34c410589d3644e3aea5096df171fa1f9f03
andre <aharris1727@gmail.com>
2018-01-25 15:34:59 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+
+
+// var play = document.getElementById("play")
+var pause = document.getElementById('pauseButton')
+var next = document.getElementById('nextButton')
+
+
+
+// function Music(){
+// 	this.song = [];
+// 	this.addSong = function(x){
+// 		console.log(x);
+// 		this.song.push(x)
+// 	}
+// }
+
+// var playSongs = new Music("")
+
+// function playAudio() { 
+//    var x = document.getElementById("songs");
+   
+//    document.getElementById('displayBox').innerHTML = x;
+//     x.play()
+// } 
+// function stopAudio(){
+// 	var x = document.getElementById("songs");
+// 	x.pause();
+//     x.currentTime = 0;
+	
+
+// }
+
+function Songs(){
+	this.song = [];
+
+}
+
+
+
+
+
+
+
+
+
+var audio = new Audio("audio/How_it_Began.mp3","audio/Galactic_Damages.mp3","audio/Half_Pipe.mp3");
+audio.play();
+
+
+
+
+
